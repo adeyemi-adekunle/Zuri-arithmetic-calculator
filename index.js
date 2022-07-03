5d4f6e74903a5ffb89934af338c6fec9d58c701f
Adeyemi Adekunle <bamidek@gmail.com>
2022-07-03 12:13:41 +0100

    commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// This program is a simple arithmetric calculator
+// This program is a simple arithmetic calculator
 //Declaring the initial variables to be zero
  let num1 = 0
  let num2 = 0
  let result = 0
 
-// the code below to perform  simple the arithmetric calculation
+// the code below to perform  simple the arithmetic calculation
 
   num1 =  parseFloat(prompt("Enter the first number: "))
  const operator = prompt("Enter the Operator ( + , - , / , x ):")
